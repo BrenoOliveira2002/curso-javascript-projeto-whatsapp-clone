1e227ef0f81869ec9b6d324970bda20f716b0936
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-05-25 12:21:24 -0300

    Revert "Adding WebPack"
    This reverts commit cf82ea6dec1fc6d78f5c4eb13c3ebfd79a7bb77c.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
 
         filename:'bundle.js',
-        path: path.resolve(__   dirname, '/dist'),
+        path: path.resolve(__dirname, '/dist'),
         publicPath: 'dist'
     }
 }
