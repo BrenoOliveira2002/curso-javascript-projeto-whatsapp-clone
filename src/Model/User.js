eb01acf0b5cf45dc3e075be731ef25726021ddc2
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-05-31 10:38:35 -0300

    listing contacts

--- src/Model/User.js
+++ src/Model/User.js
@@ -55,6 +55,15 @@ export class User extends Model {
 
       }
 
+      static getContactsRef(id){
+
+        return User.getRef()
+        .doc(id).
+        collection('contacts')
+
+
+      }
+
       static findByEmail(email){
 
         return User.getRef().doc(email);
@@ -62,10 +71,32 @@ export class User extends Model {
 
       addContact(contact) {
 
-        return User.getRef().doc(this.email).collection('contacts').doc(btoa(contact.email)).set(contact.toJSON());
+        return User.getContactsRef(this.email).doc(btoa(contact.email)).set(contact.toJSON());
 
 
     }
 
+    getContacts() {
 
-    }
+      return new Promise((s, f) => {
+          User.getContactsRef(this.email).onSnapshot(docs => {
+
+              let contacts = [];
+
+              docs.forEach(doc => {
+
+                  let data = doc.data();
+
+                  data.id = doc.id;
+
+                  contacts.push(data);
+              });
+
+              this.trigger('contactschange', docs);
+
+              s(contacts);
+          });
+      });
+  }
+
+}
